378a0872f2c37f1bd284d94cc4bd379c4cab6133
John Henry Thompson <jht1493@gmail.com>
2023-02-14 15:56:06 -0500

    Update Contents.swift

--- Closure-Ints-Strings.playground/Pages/Dictionary.xcplaygroundpage/Contents.swift
+++ Closure-Ints-Strings.playground/Pages/Dictionary.xcplaygroundpage/Contents.swift
@@ -1,12 +1,12 @@
 // dictionary
 
-var d1 = [1: 10, 2: 20]
+var d1 = ["1": "10", "2": "20"]
 print("d1",d1,"\n")
 
 // dictionary instances are not shared
 var d2 = d1
-d2[1] = 11
-d2[2] = 2000
+d2["1"] = "11"
+d2["2"] = "2000"
 
 print("d1",d1)
 print("d2",d2)
