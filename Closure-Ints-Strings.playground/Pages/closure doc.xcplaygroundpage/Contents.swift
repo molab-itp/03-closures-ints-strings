c79f3b425aac4912fb2bd5a4ac1f216cf2f94a85
John Henry Thompson <jht9629@nyu.edu>
2024-09-20 00:20:30 -0400

    Update Contents.swift

--- Closure-Ints-Strings.playground/Pages/closure doc.xcplaygroundpage/Contents.swift
+++ Closure-Ints-Strings.playground/Pages/closure doc.xcplaygroundpage/Contents.swift
@@ -7,3 +7,15 @@ if let i = students.firstIndex(where: { $0.hasPrefix("A") }) {
 }
 
 // where: { $0.hasPrefix("A") }
+
+// https://docs.swift.org/swift-book/documentation/the-swift-programming-language/closures/
+
+let names = ["Chris", "Alex", "Ewa", "Barry", "Daniella"]
+
+func backward(_ s1: String, _ s2: String) -> Bool {
+    return s1 > s2
+}
+var reversedNames = names.sorted(by: backward)
+// reversedNames is equal to ["Ewa", "Daniella", "Chris", "Barry", "Alex"]
+
+print("reversedNames \(reversedNames)")
